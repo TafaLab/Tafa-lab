e179d295a9a1d9e768c41a6743fd8d5bc3a1ce2f
Talifa S. <suyunova.talifa@gmail.com>
2026-09-21 22:46:57 +0700

    Combine NYC restaurant lead chunks

---
+++ app/[locale]/stk-admin/nyc-restaurant-seed.ts
@@ -0,0 +1,19 @@
+import { nycRestaurantLeadSeed01 } from "./nyc-restaurant-seed-01";
+import { nycRestaurantLeadSeed02 } from "./nyc-restaurant-seed-02";
+import { nycRestaurantLeadSeed03 } from "./nyc-restaurant-seed-03";
+import { nycRestaurantLeadSeed04 } from "./nyc-restaurant-seed-04";
+import { nycRestaurantLeadSeed05 } from "./nyc-restaurant-seed-05";
+import { nycRestaurantLeadSeed06 } from "./nyc-restaurant-seed-06";
+import { nycRestaurantLeadSeed07 } from "./nyc-restaurant-seed-07";
+import { nycRestaurantLeadSeed08 } from "./nyc-restaurant-seed-08";
+import { nycRestaurantLeadSeed09 } from "./nyc-restaurant-seed-09";
+import { nycRestaurantLeadSeed10 } from "./nyc-restaurant-seed-10";
+import { nycRestaurantLeadSeed11 } from "./nyc-restaurant-seed-11";
+import { nycRestaurantLeadSeed12 } from "./nyc-restaurant-seed-12";
+import { nycRestaurantLeadSeed13 } from "./nyc-restaurant-seed-13";
+import { nycRestaurantLeadSeed14 } from "./nyc-restaurant-seed-14";
+import { nycRestaurantLeadSeed15 } from "./nyc-restaurant-seed-15";
+import { nycRestaurantLeadSeed16 } from "./nyc-restaurant-seed-16";
+import { nycRestaurantLeadSeed17 } from "./nyc-restaurant-seed-17";
+
+export const nycRestaurantLeadSeed = [...nycRestaurantLeadSeed01,...nycRestaurantLeadSeed02,...nycRestaurantLeadSeed03,...nycRestaurantLeadSeed04,...nycRestaurantLeadSeed05,...nycRestaurantLeadSeed06,...nycRestaurantLeadSeed07,...nycRestaurantLeadSeed08,...nycRestaurantLeadSeed09,...nycRestaurantLeadSeed10,...nycRestaurantLeadSeed11,...nycRestaurantLeadSeed12,...nycRestaurantLeadSeed13,...nycRestaurantLeadSeed14,...nycRestaurantLeadSeed15,...nycRestaurantLeadSeed16,...nycRestaurantLeadSeed17];
