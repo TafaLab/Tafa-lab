90d980b95c762e092041c373c13cd769d28ddcd7
Talifa S. <suyunova.talifa@gmail.com>
2026-09-21 22:51:45 +0700

    Replace malformed NYC bakery and coffee seed files

--- app/[locale]/stk-admin/nyc-bakery-coffee-seed.ts
+++
@@ -1,6 +0,0 @@
-import { nycBakeryCoffeeLeadSeed01 } from "./nyc-bakery-coffee-seed-01";
-import { nycBakeryCoffeeLeadSeed02 } from "./nyc-bakery-coffee-seed-02";
-import { nycBakeryCoffeeLeadSeed03 } from "./nyc-bakery-coffee-seed-03";
-import { nycBakeryCoffeeLeadSeed04 } from "./nyc-bakery-coffee-seed-04";
-
-export const nycBakeryCoffeeLeadSeed = [...nycBakeryCoffeeLeadSeed01, ...nycBakeryCoffeeLeadSeed02, ...nycBakeryCoffeeLeadSeed03, ...nycBakeryCoffeeLeadSeed04];
